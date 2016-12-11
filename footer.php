650c0dd6bdc47a0cb355c48d09733ff1f0921448
Jules <juliopatriciodasilva@gmail.com>
2016-12-11 22:49:25 +0000

    footer.php

---
+++ footer.php
@@ -0,0 +1,28 @@
+
+
+<div id="footer">
+   
+    <div id="manuales">
+        
+        <ul>
+            <li><h3>Documentacion</h3></li>
+            <li><a href="http://www.summarg.com/2013/taller-crear-un-theme-wordpress-desde-cero/"> Como crear un tema de WordPress</a></li>
+            <li><a href="https://wordpress.org/download/">Descarga WordPress</a></li>
+
+        </ul>
+        
+        
+    </div>
+    
+    
+    
+</div>
+
+
+
+
+
+
+<?php wp_footer(); ?>
+</body>
+</html>
